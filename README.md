c008c114a97f1f83e41b5b59e55f824d288676f0
an <d_1206@qq.com>
2024-04-12 21:37:08 +0800

    mf

--- README.md
+++ README.md
@@ -13,6 +13,6 @@ cBezier.py 基于贝塞尔曲线实现模拟人手动滑动的轨迹
 ```
 https://www.tampermonkey.net/
 tampermonkey_files 油猴脚本
-
-    chaoxing_video_no_stop.js 超星 - 禁止暂停视频
+    coze.js                      coze 调整窗口大小
+    chaoxing_video_no_stop.js    超星 - 禁止暂停视频
 ```
---
+++ tampermonkey_files/coze.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name         Coze
+// @namespace    http://tampermonkey.net/
+// @version      2024-02-12
+// @description  try to take over the world!
+// @author       You
+// @match        https://www.coze.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=coze.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // 添加 CSS 设置
+    var style = document.createElement('style');
+    style.innerHTML = `
+        .sidesheet-container {
+            grid-template-columns: 13fr 13fr 48fr !important;
+        }
+    `;
+    document.head.appendChild(style);
+})();
